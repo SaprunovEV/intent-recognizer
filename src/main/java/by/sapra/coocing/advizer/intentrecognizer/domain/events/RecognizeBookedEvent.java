5521ac98ff4e96ccc84bce138f1dcc9e01e244bb
Евгений Сапрунов <sapra10062018@gmail.com>
2025-05-20 16:18:58 +0300

    Идея посоветовала использовать рекорд.

--- src/main/java/by/sapra/coocing/advizer/intentrecognizer/domain/events/RecognizeBookedEvent.java
+++ src/main/java/by/sapra/coocing/advizer/intentrecognizer/domain/events/RecognizeBookedEvent.java
@@ -1,9 +1,4 @@
 package by.sapra.coocing.advizer.intentrecognizer.domain.events;
 
-public class RecognizeBookedEvent {
-    private final RecognizeBookedEventData recognizeBookedEventData;
-
-    public RecognizeBookedEvent(RecognizeBookedEventData recognizeBookedEventData) {
-        this.recognizeBookedEventData = recognizeBookedEventData;
-    }
+public record RecognizeBookedEvent(RecognizeBookedEventData recognizeBookedEventData) {
 }
